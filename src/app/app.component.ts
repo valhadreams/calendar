1829ed4ba2d87b8a97ed3cb80a12dcdc64b2fc26
valhadreams <valhadreams@gmail.com>
2017-12-27 17:25:10 +0900

    - Calendar UI - Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import {CalendarService, Day} from './calendar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CalendarService]
 })
 export class AppComponent {
-  title = 'app';
+
+  currentDate : Date;
+  dayOfWeeks;
+  days : Array<Day>;
+
+  constructor(private calendarService : CalendarService){
+    this.dayOfWeeks = this.calendarService.getDayOfWeeks();
+    this.currentDate = new Date();
+    const thisYear = this.currentDate.getFullYear();
+    const thisMonth = this.currentDate.getMonth();
+
+    this.setMonthData(thisYear, thisMonth);
+  }
+
+  setMonthData(year : number, month : number){
+    this.days = new Array();
+    let lastDateOfMonth = new Date(year, month + 1, 0);
+
+    const lastDay = lastDateOfMonth.getDate();
+
+    for(let i = 0; i < lastDay; i ++ ) {
+
+      const date = new Date(year, month, i + 1);
+      const events = ['weight', 'running'];
+      const dayObj = new Day(date, events);
+      if(i === 0) {
+        for (let j = 0; j < dayObj.date.getDay(); j++) {
+          this.days.push(new Day(null, []));
+        }
+      }
+      this.days.push(dayObj);
+      if(i === lastDay - 1) {
+        for (let j = dayObj.date.getDay() + 1; j < this.dayOfWeeks.length; j++) {
+          this.days.push(new Day(null, []));
+        }
+      }
+    }
+  }
+
+  clickPrevBtn(){
+    this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1);
+    this.setMonthData(this.currentDate.getFullYear(), this.currentDate.getMonth());
+  }
+
+  clickNextBtn(){
+    this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1);
+    this.setMonthData(this.currentDate.getFullYear(), this.currentDate.getMonth());
+  }
+
+  clickAddEvent(day : Object){
+    console.log(day);
+  }
+
+  clickEditEvent(day : Object){
+    console.log(day);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CdkTableModule} from "@angular/cdk/table";
+import {
+  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule,
+  MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatExpansionModule, MatGridListModule,
+  MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule,
+  MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule, MatSnackBarModule, MatSortModule,
+  MatStepperModule, MatTableModule, MatTabsModule, MatTooltipModule
+} from "@angular/material";
 
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+  ]
+})
+export class DemoMaterialModule {}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule, DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
